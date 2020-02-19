1f47ba6af66d9ea91e8cba48addaae30403214a1
coder7100 <a.aundhkar7100@gmail.com>
2020-02-19 15:17:36 +0530

    commit

--- movies/urls.py
+++ movies/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="movie_index"),
-    path("< movie_id >", views.detail, name="movie_detail")
+    path("<movie_id>", views.detail, name="movie_detail")
 ]

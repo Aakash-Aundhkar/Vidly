74fe15536d8030e33970015dcd801bdf5616139f
coder7100 <a.aundhkar7100@gmail.com>
2020-02-21 17:08:33 +0530

    adding home page

--- vidly/urls.py
+++ vidly/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api.models import movieresource
+from . import views
 
 movie_resource = movieresource()
 
 urlpatterns = [
+    path("", views.home),
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('api/', include(movie_resource.urls))
